3b0e618e8cf4fc22acd4498795f9b2ca7d60c940
Yuuki Shimizu <shimizu@cosmoway.net>
2016-05-02 19:19:46 +0900

    create app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+var WebSocketServer = require('ws').Server
+    , http = require('http')
+    , express = require('express')
+    , app = express();
+
+var server = http.createServer(app);
+var wss = new WebSocketServer({server: server});
+
+var connections = [];
+
+wss.on('connection', function (ws) {
+    connections.push(ws);
+
+    ws.on('close', function () {
+        connections = connections.filter(function (conn, i) {
+            return (conn !== ws);
+        });
+    });
+
+    ws.on('message', function (message) {
+        // TODO:
+    });
+});
+
+server.listen(process.env.PORT || 5000);
