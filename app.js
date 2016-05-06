e256c6cdc97874f494103dd19397be7d0a9a2c2a
Yuuki Shimizu <shimizu@cosmoway.net>
2016-05-06 09:01:25 +0900

    クライアントが接続されたときの処理を実装
    cf) https://github.com/cosmoway/smalo-server/wiki/websocket
    - 接続時、UUID が送信された connection をブロードキャスト対象として管理
    - 施錠/解錠リクエストを受けて状態をブロードキャスト
    - ブロードキャスト処理

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+/**
+ * smalo-server app.js
+ */
+
 var WebSocketServer = require('ws').Server
     , http = require('http')
     , express = require('express')
@@ -10,17 +14,74 @@ var wss = new WebSocketServer({server: server});
 var connections = [];
 
 wss.on('connection', function (ws) {
-    connections.push(ws);
 
     ws.on('close', function () {
         connections = connections.filter(function (conn, i) {
             return (conn !== ws);
         });
+        console.log('[DEBUG] close connection. count: %d'.replace(/%d/, connections.length));
     });
 
     ws.on('message', function (message) {
-        // TODO:
+        var jsonObject;
+        try {
+            jsonObject = JSON.parse(message);
+        } catch (e) {
+            console.log(e);
+            return;
+        }
+
+        try {
+            var uuid = jsonObject['uuid'];
+            var command = jsonObject['command'];
+
+            if (uuid != null) {
+                // C-01. 接続時
+                // TODO: UUID 認証をする
+                if (connections.indexOf(ws) < 0) {
+                    connections.push(ws);
+                    console.log('[DEBUG] increase connection count: %d'.replace(/%d/, connections.length));
+                }
+
+            } else if (command != null) {
+                if (command == 'lock') {
+                    // C-02. 施錠リクエスト
+                    console.log('[DEBUG] receive lock request.');
+
+                    // TODO: Edison に施錠リクエストを送る
+                    setTimeout(function () {
+                        broadcast({state: 'lock'});
+                    }(), 1000);
+
+                } else if (command == 'unlock') {
+                    // C-03. 解錠リクエスト
+                    console.log('[DEBUG] receive unlock request.');
+
+                    // TODO: Edison に解錠リクエストを送る
+                    setTimeout(function () {
+                        broadcast({state: 'unlock'});
+                    }(), 1000);
+
+                }
+            }
+        } catch (e) {
+            console.log(e);
+            return;
+        }
     });
 });
+
+/**
+ * 接続されているクライアントにデータを broadcast する
+ *
+ * @param object
+ */
+function broadcast(object) {
+    var message = JSON.stringify(object);
+    connections.forEach(function (con, i) {
+        con.send(message);
+    });
+    console.log('[DEBUG] broadcast message: %s'.replace(/%s/, message));
+}
 
 server.listen(process.env.PORT || 5000);
