c1c48fb1a3b3e47d0b1800a4dc7f968534717243
Kazutoshi Shinkai <gokazy@gmail.com>
2016-05-11 11:40:09 +0900

    エラーログを変更しました。

--- routes/devices.js
+++ routes/devices.js
@@ -45,17 +45,28 @@ router.post(/^\/v1\/devices$/, function(req, res, next){
     select_devices = mysql.format(select_devices, [device_uuid]);
     connection.query(select_devices, function(err, rows, fields){
         if (err) {
-            return next(new Error('database error.'));
+            // log.warn('Database Error. Message: %s. Query: "%s".', err.message, select_devices);
+
+            error = new Error('Database error has occurred.');
+            error.status = 500;
+            error.status_message = 'Internal Server Error';
+            return next(error);
         }
         if (rows.length === 0) {
             // データがなければ、INSERT
+            // log.debug('Select Device: 0 Row. Query: "%s".', select_devices);
+
             now = moment().format('YYYY-MM-DD HH:mm:ss');
             var insert_devices = 'INSERT INTO devices (uuid, name, key_lock_code, created, updated) VALUES (?, ?, ?, ?, ?)';
             insert_devices = mysql.format(insert_devices, [device_uuid, device_name, 'key', now, now]);
             connection.query(insert_devices, function(err, results){
                 if (err) {
-                    console.error(err);
-                    return next(new Error('noooooooooooooooooooooo'));
+                    //log.warn('Database Error. Message: %s. Query: "%s".', err.message, insert_devices);
+
+                    error = new Error('Database error has occurred.');
+                    error.status = 500;
+                    error.status_message = 'Internal Server Error';
+                    return next(error);
                 }
                 return res.status(204).end();
             });
@@ -64,16 +75,22 @@ router.post(/^\/v1\/devices$/, function(req, res, next){
         if (rows.length === 1) {
             // データが存在しかつ端末名が異なる場合、UPDATE（仮登録状態）にする。
             if (rows[0].name === device_name) {
+                // log.debug('same device name.');
                 console.log('same device name.');
                 return res.status(204).end();
             }
+            // log.debug('change device name.');
             now = moment().format('YYYY-MM-DD HH:mm:ss');
             var update_devices = 'UPDATE devices SET name = ?, is_registered = ?, is_enabled = ?, updated = ? WHERE uuid = ?';
             update_devices = mysql.format(update_devices, [device_name, 0, 0, now, device_uuid]);
             connection.query(update_devices, function(err, results){
                 if (err) {
-                    console.error(err);
-                    return next(new Error('errooooooooooooooooooooor'));
+                    //log.warn('Database Error. Message: %s. Query: "%s".', err.message, update_devices);
+
+                    error = new Error('Database error has occurred.');
+                    error.status = 500;
+                    error.status_message = 'Internal Server Error';
+                    return next(error);
                 }
                 return res.status(204).end();
             });
