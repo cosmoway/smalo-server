5001d2871561b46de611078dba91ced44d2ed52a
Kazutoshi Shinkai <gokazy@gmail.com>
2016-05-13 12:18:21 +0900

    本登録や有効化の処理を追加しました。

--- routes/admin-devices.js
+++ routes/admin-devices.js
@@ -3,6 +3,7 @@ var debug = require('debug')('smalo-server:admin:devices');
 var express = require('express');
 var mysql = require('mysql');
 var moment = require('moment');
+var Device = require('../device.js').Device;
 // TODO: mysqlへの接続情報は、configで管理するように。
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -60,6 +61,78 @@ router.get(/^\/devices$/, function(req, res, next){
         params['devices'] = results;
         res.render('admin-devices', params);
     });
+});
+
+// 端末本登録
+router.post(/^\/devices\/(?:([-\w]+))\/registered$/, function(req, res, next){
+    var device_uuid = req.params[0];
+    var now = moment().format('YYYY-MM-DD HH:mm:ss');
+    var error;
+
+    var update_devices = 'UPDATE devices SET is_registered = 1, is_enabled = 1, updated = ? WHERE uuid = ?';
+    update_devices = mysql.format(update_devices, [now, device_uuid]);
+    connection.query(update_devices, function(err, results){
+        debug('[QUERY] ' + update_devices);
+        if (err) {
+            //log.warn('Database Error. Message: %s. Query: "%s".', err.message, update_devices);
+            debug('Database Error. Message: ' + err.message);
+
+            error = new Error('Database error has occurred.');
+            error.status = 500;
+            error.status_message = 'Internal Server Error';
+            return next(error);
+        }
+    });
+    Device.emitChange();
+    return res.status(204).end();
+});
+
+// 端末有効化
+router.post(/^\/devices\/(?:([-\w]+))\/enabled$/, function(req, res, next){
+    var device_uuid = req.params[0];
+    var now = moment().format('YYYY-MM-DD HH:mm:ss');
+    var error;
+
+    var update_devices = 'UPDATE devices SET is_enabled = 1, updated = ? WHERE uuid = ?';
+    update_devices = mysql.format(update_devices, [now, device_uuid]);
+    connection.query(update_devices, function(err, results){
+        debug('[QUERY] ' + update_devices);
+        if (err) {
+            //log.warn('Database Error. Message: %s. Query: "%s".', err.message, update_devices);
+            debug('Database Error. Message: ' + err.message);
+
+            error = new Error('Database error has occurred.');
+            error.status = 500;
+            error.status_message = 'Internal Server Error';
+            return next(error);
+        }
+    });
+    Device.emitChange();
+    return res.status(204).end();
+});
+
+// 端末無効化
+router.post(/^\/devices\/(?:([-\w]+))\/disabled$/, function(req, res, next){
+    var device_uuid = req.params[0];
+    var now = moment().format('YYYY-MM-DD HH:mm:ss');
+    var error;
+
+    var update_devices = 'UPDATE devices SET is_enabled = 0, updated = ? WHERE uuid = ?';
+    update_devices = mysql.format(update_devices, [now, device_uuid]);
+    connection.query(update_devices, function(err, results){
+        debug('[QUERY] ' + update_devices);
+        if (err) {
+            //log.warn('Database Error. Message: %s. Query: "%s".', err.message, update_devices);
+            debug('Database Error. Message: ' + err.message);
+
+            error = new Error('Database error has occurred.');
+            error.status = 500;
+            error.status_message = 'Internal Server Error';
+            return next(error);
+        }
+    });
+    Device.emitChange();
+    return res.status(204).end();
 });
 
 module.exports = router;
