17bedf75840eb8ef84ea803cb41af50025457756
Kazutoshi Shinkai <gokazy@gmail.com>
2016-05-17 00:45:50 +0900

    変数名を修正しました。

--- routes/devices.js
+++ routes/devices.js
@@ -10,15 +10,15 @@ var connection = db.connection;
 
 var router = express.Router();
 router.post(/^\/v1\/devices$/, function(req, res, next){
-    var device_uuid = req.body.uuid;
-    var device_name = req.body.name;
+    var deviceUuid = req.body.uuid;
+    var deviceName = req.body.name;
     var isLock = req.body.isLock || false;
     var keyLockCode = isLock ? 'lock' : 'key';
     var now = moment().format('YYYY-MM-DD HH:mm:ss');
     var error;
 
     // バリデーション
-    if (device_uuid === undefined || device_name === undefined) {
+    if (deviceUuid === undefined || deviceName === undefined) {
         debug('Validation Error: UUID or Name undefined.');
 
         error = new Error('validation error: required uuid and name.');
@@ -26,7 +26,7 @@ router.post(/^\/v1\/devices$/, function(req, res, next){
         error.status_message = 'Bad Request';
         return next(error);
     }
-    if (device_uuid.length === 0 || device_uuid.length > 36) {
+    if (deviceUuid.length === 0 || deviceUuid.length > 36) {
         debug('Validation Error: UUID length error.');
 
         error = new Error('validation error: uuid length error.');
@@ -34,7 +34,7 @@ router.post(/^\/v1\/devices$/, function(req, res, next){
         error.status_message = 'Bad Request';
         return next(error);
     }
-    if (device_name.length === 0 || device_name.length > 50) {
+    if (deviceName.length === 0 || deviceName.length > 50) {
         debug('Validation Error: Name length error.');
 
         error = new Error('validation error: name length error.');
@@ -45,7 +45,7 @@ router.post(/^\/v1\/devices$/, function(req, res, next){
 
     // 登録データを確認する。
     var select_devices = 'SELECT uuid, name, is_registered, is_enabled FROM devices WHERE uuid = ?';
-    select_devices = mysql.format(select_devices, [device_uuid]);
+    select_devices = mysql.format(select_devices, [deviceUuid]);
     connection.query(select_devices, function(err, rows, fields){
         debug('[QUERY] ' + select_devices);
         if (err) {
@@ -64,7 +64,7 @@ router.post(/^\/v1\/devices$/, function(req, res, next){
 
             now = moment().format('YYYY-MM-DD HH:mm:ss');
             var insert_devices = 'INSERT INTO devices (uuid, name, key_lock_code, created, updated) VALUES (?, ?, ?, ?, ?)';
-            insert_devices = mysql.format(insert_devices, [device_uuid, device_name, keyLockCode, now, now]);
+            insert_devices = mysql.format(insert_devices, [deviceUuid, deviceName, keyLockCode, now, now]);
             connection.query(insert_devices, function(err, results){
                 debug('[QUERY] ' + insert_devices);
                 if (err) {
@@ -84,7 +84,7 @@ router.post(/^\/v1\/devices$/, function(req, res, next){
         if (rows.length === 1) {
             // データが存在しかつ端末名が異なる場合、UPDATE（仮登録状態）にする。
             debug('Select Device: 1 Row. Query: ' + select_devices);
-            if (rows[0].name === device_name) {
+            if (rows[0].name === deviceName) {
                 // log.debug('same device name.');
                 debug('Found Same device name!');
 
@@ -95,7 +95,7 @@ router.post(/^\/v1\/devices$/, function(req, res, next){
             debug('Change device name!');
             now = moment().format('YYYY-MM-DD HH:mm:ss');
             var update_devices = 'UPDATE devices SET name = ?, key_lock_code = ?, is_registered = ?, is_enabled = ?, updated = ? WHERE uuid = ?';
-            update_devices = mysql.format(update_devices, [device_name, keyLockCode, 0, 0, now, device_uuid]);
+            update_devices = mysql.format(update_devices, [deviceName, keyLockCode, 0, 0, now, deviceUuid]);
             connection.query(update_devices, function(err, results){
                 debug('[QUERY] ' + update_devices);
                 if (err) {
